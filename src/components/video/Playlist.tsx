0f8ad2e5ad46d6cf606d34f36fbe8beda4b2195a
prateek-jha <prtkjha01@gmail.com>
2024-02-15 20:37:23 +0530

    drag and drop implemented on playlist

--- src/components/video/Playlist.tsx
+++ src/components/video/Playlist.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState, useRef } from "react";
 import { videos } from "../../utils/constants";
 
 const Video: FC<{ video: any; isLast: boolean }> = ({ video, isLast }) => {
@@ -24,16 +24,32 @@ const Video: FC<{ video: any; isLast: boolean }> = ({ video, isLast }) => {
 };
 
 const Playlist: FC<{ handleSetVideo: Function }> = ({ handleSetVideo }) => {
+  const [playlist, setPlaylist] = useState(videos);
+
+  const dragVideo = useRef<number>(0);
+  const draggedOverVideo = useRef<number>(0);
+
+  function handleSort() {
+    const playlistClone = [...playlist];
+    const temp = playlistClone[dragVideo.current];
+    playlistClone[dragVideo.current] = playlistClone[draggedOverVideo.current];
+    playlistClone[draggedOverVideo.current] = temp;
+    setPlaylist(playlistClone);
+  }
+
   return (
     <>
       <div className="playlist-container max-h-[calc(100vh-200px)] overflow-y-scroll w-full lg:w-[30%] mt-10 lg:mt-0 pt-0 p-5">
-        {videos.map((video, index) => (
+        {playlist.map((video, index) => (
           <div
             key={video.sources}
             className="cursor-pointer"
+            draggable
+            onDragStart={() => (dragVideo.current = index)}
+            onDragEnter={() => (draggedOverVideo.current = index)}
+            onDragEnd={handleSort}
+            onDragOver={(e) => e.preventDefault()}
             onClick={() => {
-              //   console.log(video.sources);
-
               handleSetVideo(video);
             }}
           >
